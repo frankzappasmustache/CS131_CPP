64872539631694093c10d4b768e95ed80e6baa17
frankzappasmustache <dustin.t.mcclure@gmail.com>
2020-10-10 20:03:31 -0700

    pushing new project files for lab 2

---
+++ assignments/Lab_3_heartRate/HeartRates.cpp
@@ -0,0 +1,10 @@
+/*
+    Project Name: CS131_CPP
+    Sub-project: Labs
+    File Name: HeartRates.cpp
+    Author: Dustin McClure
+    Lab: Lab 2 - Target Heart-Rate Calculator
+    Modified Date: 10/10/2020
+
+    {PROGRAM DESCRIPTION}
+ //*/
---
+++ assignments/Lab_3_invoice/Invoice.cpp
@@ -0,0 +1,10 @@
+/*
+    Project Name: CS131_CPP
+    Sub-project: Labs
+    File Name: Invoice.cpp
+    Author: Dustin McClure
+    Lab: Lab 2 - Invoice
+    Modified Date: 10/10/2020
+
+    {PROGRAM DESCRIPTION}
+ //*/
