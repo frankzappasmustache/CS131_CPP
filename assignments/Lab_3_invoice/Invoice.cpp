6dfbdb05d2685039ec8eb36c5affeda8c5155052
frankzappasmustache <dustin.t.mcclure@gmail.com>
2020-10-14 13:24:47 -0700

    pushing finished invoice header and source code file

--- assignments/Lab_3_invoice/Invoice.cpp
+++ assignments/Lab_3_invoice/Invoice.cpp
@@ -4,7 +4,62 @@
     File Name: Invoice.cpp
     Author: Dustin McClure
     Lab: Lab 2 - Invoice
-    Modified Date: 10/10/2020
+    Modified Date: 10/11/2020
 
     {PROGRAM DESCRIPTION}
  //*/
+
+#include <iostream>
+#include <string>
+#include "Invoice.h"
+
+using namespace std;
+
+int main()
+{
+  Invoice myInvoice;
+
+  cout << "The current part number is: " << myInvoice.getPartNumber() << "\n\n";
+  cout << "The description for this part is: " << myInvoice.getPartDescription() << "\n\n";
+  cout << "The quantity of this part on the invoice is: " << myInvoice.getItmQuantity() << "\n\n";
+  cout << "The price per each for this part is: " << myInvoice.getItmPrice() << "\n\n";
+
+  cout << "Please enter the part number you would like to add to this invoice: ";
+
+  string part;
+
+  getline(cin, part);
+
+  myInvoice.setPartNumber(part);
+
+  cout << "\nPlease enter the part description you would like to appear on the invoice: ";
+
+  string description;
+
+  getline(cin, description);
+
+  myInvoice.setPartDescription(description);
+
+  cout << "\nPlease enter the quantity of this part to be billed for: ";
+
+  int quantity;
+
+  cin >> quantity;
+
+  myInvoice.setItmQuantity(quantity);
+
+  cout << "\nPlease enter the standard price per part billed: ";
+
+  int price;
+
+  cin >> price;
+
+  myInvoice.setItmPrice(price);
+
+  cout << "\nThe total invoice amount for " << quantity << " of ";
+  cout << "part number " << part << "(" << description << ") is "; 
+  cout << "$" << myInvoice.getInvoiceAmount() << "." << endl;
+
+  return 0;
+
+}
---
+++ assignments/Lab_3_invoice/Invoice.h
@@ -0,0 +1,58 @@
+/*
+    Project Name: CS131_CPP
+    Sub-project: Labs
+    File Name: Invoice.h
+    Author: Dustin McClure
+    Lab: Lab 2 - Invoice
+    Modified Date: 10/11/2020
+
+    {PROGRAM DESCRIPTION}
+ //*/
+
+#include <iostream>
+#include <string>
+
+#ifndef INVOICE_H_INCLUDED
+#define INVOICE_H_INCLUDED
+
+class Invoice {
+ public:
+  // Invoice(string mpartNumber, string mpartDescription, int mitmQuantity, int
+  // mitmPrice)
+  /*{
+     partNumber{mpartNumber};
+     partDescription{mpartDescription};
+     itmQuantity{mitmQuantity};
+     itmPrice{mitmPrice}string;
+  };
+  //*/
+  void setPartNumber(std::string mpartNumber) { partNumber = mpartNumber; }
+
+  void setPartDescription(std::string mpartDescription) {
+    partDescription = mpartDescription;
+  }
+
+  void setItmQuantity(int mitmQuantity) { itmQuantity = mitmQuantity; }
+
+  void setItmPrice(int mitmPrice) { itmPrice = mitmPrice; }
+
+  std::string getPartNumber() { return partNumber; }
+  std::string getPartDescription() { return partDescription; }
+  int getItmQuantity() { return control(itmQuantity); }
+  int getItmPrice() { return control(itmPrice); }
+  int getInvoiceAmount() { return control(itmQuantity * itmPrice); }
+  int control(int x) {
+    if (x < 0) {
+      return 0;
+    } else {
+      return x;
+    }
+  }
+
+ private:
+  std::string partNumber;       // data member
+  std::string partDescription;  // data member
+  int itmQuantity{0};           // data member
+  int itmPrice{0};              // data member
+};
+#endif
