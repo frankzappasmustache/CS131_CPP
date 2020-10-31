6779f5432f9289ba399cb2bff838c2ed6567d15e
frankzappasmustache <dustin.t.mcclure@gmail.com>
2020-10-30 22:17:56 -0700

    pushing updated source and header files for avgOfIntegers

--- assignments/Lab_5a_smallestValue/avgOfIntegers.cpp
+++ assignments/Lab_5a_smallestValue/avgOfIntegers.cpp
@@ -8,3 +8,91 @@
 
     The class implementation of the avgOfIntegers class
 */
+
+#include "avgOfIntegers.h"
+
+#include <math.h>
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  avgOfIntegers intAverage(int numberCount, double average, int numOne,
+                           int numTwo, int numThree, int numFour, int numFive);
+  // Declarations
+  int numberCount{0};
+  double average{0};
+  int numOne{0};
+  int numTwo{0};
+  int numThree{0};
+  int numFour{0};
+  int numFive{0};
+
+  while (numberCount != 10) {
+    cout << "Please enter a number of integers for which you would like to find"
+         << " the average. You can calculate for 3, 4, or 5 integers. Please "
+            "enter 10 to exit:\n";
+    cin >> numberCount;
+
+    if (numberCount == 10) break;
+
+    switch (numberCount) {
+      case 3:
+        cout << "Please enter first number:\n";
+        cin >> numOne;
+        cout << "Please enter second number:\n";
+        cin >> numTwo;
+        cout << "Please enter third number:\n";
+        cin >> numThree;
+        if (0 <= numOne && numTwo && numThree >= 100) {
+          average = (numOne + numTwo + numThree) / 3;
+          return average;
+        } else {
+          cout << "Please enter a number between 0 and 100\n";
+          break;
+        }
+        break;
+      case 4:
+        cout << "Please enter first number:\n";
+        cin >> numOne;
+        cout << "Please enter second number:\n";
+        cin >> numTwo;
+        cout << "Please enter third number:\n";
+        cin >> numThree;
+        cout << "Please enter fourth number:\n";
+        cin >> numFour;
+        if (0 <= numOne && numTwo && numThree && numFour >= 100) {
+          average = (numOne + numTwo + numThree + numFour) / 4;
+          return average;
+        } else {
+          cout << "Please enter a number between 0 and 100\n";
+          break;
+        }
+        break;
+      case 5:
+        cout << "Please enter first number:\n";
+        cin >> numOne;
+        cout << "Please enter second number:\n";
+        cin >> numTwo;
+        cout << "Please enter third number:\n";
+        cin >> numThree;
+        cout << "Please enter fourth number:\n";
+        cin >> numFour;
+        cout << "Please enter fifth number:\n";
+        cin >> numFive;
+        if (0 <= numOne && numTwo && numThree && numFour && numFive >= 100) {
+          average = (numOne + numTwo + numThree + numFour + numFive) / 5;
+          return average;
+        } else {
+          cout << "Please enter a number between 0 and 100\n";
+          break;
+        }
+        break;
+    }
+  cout << "The average of the numbers you entered is: " << average << endl;
+}
+return 0;
+}
+;
--- assignments/Lab_5a_smallestValue/avgOfIntegers.h
+++ assignments/Lab_5a_smallestValue/avgOfIntegers.h
@@ -8,3 +8,36 @@
 
     The class definition of the avgOfIntegers class
 */
+#ifndef avgOfIntegers_h
+#define avgOfIntegers_h
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <numeric>
+
+class avgOfIntegers
+{
+public:
+    avgOfIntegers(int numberCount, double average, int numOne, int numTwo, int numThree, int numFour, int numFive)
+    {
+        count = numberCount;
+        avg = average;
+        one = numOne;
+        two = numTwo;
+        three = numThree;
+        four = numFour;
+        five = numFive;
+    }
+private:
+    int count{0};
+    double avg{0};
+    int one{0};
+    int two{0};
+    int three{0};
+    int four{0};
+    int five{0};
+};
+
+#endif
